d5df6ee5d93a8bf2078ea26eb2e958efb4f4312f
HabtB <email.Habtamu@gmail.com>
2023-10-02 18:59:19 +0000

    0 get_list updated

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,5 @@
-const students = [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
-
 export default function getListStudents() {
-   return students;
+  return [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
 }
